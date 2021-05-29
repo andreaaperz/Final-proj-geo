4735cfdef66a7c60b5df1432761becb7cfee7a06
Andrea Pérez <andreaa.perz@gmail.com>
2021-05-29 14:57:30 -0500

    onSnapshot

--- js/bitacora.js
+++ js/bitacora.js
@@ -1,3 +1,7 @@
+window.onload = function () {
+    
+};
+
 const formulario = document.querySelector("#forma");
 
 formulario.addEventListener('submit', (e) => {
@@ -28,6 +32,39 @@ formulario.addEventListener('submit', (e) => {
 
     var registro = new Registro(null, formulario.titulo.value, formulario.contenido.value, check1, check2, check3);
     registro.agregar();
-    formulario.titulo.value ='';
-    formulario.contenido.value ='';
+    formulario.titulo.value = '';
+    formulario.contenido.value = '';
 });
+
+db.collection('pendientes').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if (change.type == 'added') {
+            muestraRegistros(change.doc);
+        }
+        /* else if(change.type=='removed'){
+                   console.log(change.doc.id);
+                   let valorid = document.getElementById(change.doc.id);
+                   productoslista.removeChild(valorid);
+               } */
+    });
+});
+
+function muestraRegistros(doc) {
+    console.log("muestra registros");
+    var registro = new Registro(doc.id, doc.data().titulo, doc.data().contenido, doc.data().check1, doc.data().check2, doc.data().check3);
+
+    console.log(registro.check1);
+
+    $(".card2:first").hide();
+
+    var cards2 = $(".card2:first").clone()
+    $(cards2).find(".card-title").html(registro.titulo);
+    $(cards2).find(".card-text").html(registro.contenido);
+    $(cards2).find("#item1").html(registro.check1);
+    $(cards2).find("#item2").html(registro.check2);
+    $(cards2).find("#item3").html(registro.check3);
+    $(cards2).show()
+    $(cards2).appendTo($(".row"))
+
+}
