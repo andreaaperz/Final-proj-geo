afd2953f3b0f12185e33d632997f95a27b2a6dbb
Andrea Pérez <andreaa.perz@gmail.com>
2021-06-01 12:00:56 -0500

    JavaScripts separados

---
+++ js/fetch.js
@@ -0,0 +1,15 @@
+$(".col:first").hide();
+
+fetch('datos.json')
+    .then(function (response) {
+        response.json().then(function (datos) {
+            datos.forEach(item => {
+                var cards = $(".col:first").clone()
+                $(cards).find("#imagen").attr("src", "./img/" + item.image);
+                $(cards).find(".card-title").html(item.title);
+                $(cards).find(".card-text").html(item.body);
+                $(cards).show()
+                $(cards).appendTo($(".row"))
+            });
+        });
+    });
